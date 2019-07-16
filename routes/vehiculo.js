d69f4588b9271a2efd22e104e6ce34a1647450bc
Miguel <magol@hotmail.es>
2019-07-16 18:32:08 -0500

    arreglos

--- routes/vehiculo.js
+++ routes/vehiculo.js
@@ -2,27 +2,27 @@ const express = require('express');
 const { Vehiculo } = require('../model/Vehiculo')
 const { Propietario } = require('../model/propietario')
 const app = express();
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.json('arriba')
 
 
 })
 
-app.get('/app/vehiculo/:id', function (req, res) {
+app.get('/app/vehiculo/:id', function(req, res) {
     let id = req.params.id
     Vehiculo.findAll({
-        where: {
-            PLACA: id
+            where: {
+                PLACA: id
 
-        }
-    })
+            }
+        })
         .then(buscaVehiculo => {
             Propietario.findAll({
-                where: {
-                    CEDULA: buscaVehiculo[0].PROPIETARIO
+                    where: {
+                        CEDULA: buscaVehiculo[0].PROPIETARIO
 
-                }
-            })
+                    }
+                })
                 .then(busca => {
                     buscaVehiculo[0].PROPIETARIO = busca;
                     res.json(buscaVehiculo)
@@ -32,35 +32,35 @@ app.get('/app/vehiculo/:id', function (req, res) {
 
 })
 
-app.get('/app/vehiculo/Marca/:id', function (req, res) {
+app.get('/app/vehiculo/Marca/:id', function(req, res) {
     let id = req.params.id
     Vehiculo.findAll({
-        where: {
-            CODIGO: id
+            where: {
+                CODIGO: id
 
-        }
-    })
+            }
+        })
         .then(buscaVehiculo => {
             res.json(buscaVehiculo)
 
         })
 
 })
-app.get('/app/vehiculo/Modelo/:id', function (req, res) {
+app.get('/app/vehiculo/Modelo/:id', function(req, res) {
     let id = req.params.id
     Vehiculo.findAll({
-        where: {
-            CODIGOMODELO: id
+            where: {
+                CODIGOMODELO: id
 
-        }
-    })
+            }
+        })
         .then(buscaVehiculo => {
             res.json(buscaVehiculo)
 
         })
 
 })
-app.get('/app/vehiculo', function (req, res) {
+app.get('/app/vehiculo', function(req, res) {
     Vehiculo.findAll({})
         .then(buscaVehiculo => {
             res.json(buscaVehiculo)
@@ -69,94 +69,69 @@ app.get('/app/vehiculo', function (req, res) {
 
 })
 
-app.put('/app/vehiculo/propietarios', function (req, res) {
+app.put('/app/vehiculo/propietarios', function(req, res) {
     let body = req.body;
-    
-    Vehiculo.update({ PROPIETARIO: req.body.PROPIETARIO.CEDULA}, {
-        where:{
+
+    Vehiculo.update({ PROPIETARIO: req.body.PROPIETARIO.CEDULA }, {
+        where: {
             PLACA: req.body.PLACA
         }
-    }) .then(buscaVehiculo => {
-           
-            res.json(buscaVehiculo);
+    }).then(buscaVehiculo => {
 
-        }).catch(() => {
-            return res.status(404).json();
-        })
+        res.json(buscaVehiculo);
+
+    }).catch(() => {
+        return res.status(404).json();
+    })
 
 })
 
-app.get('/app/vehiculo/propietarios/:id', function (req, res) {
+app.get('/app/vehiculo/propietarios/:id', function(req, res) {
     let id = req.params.id
-    Propietario.findAll({}).then(respuesta => {
-            var resultado = [];
-            if (respuesta.length == 0) {
-                return res.status(404).json();
+
+    var date = new Date();
+    date.setFullYear(date.getFullYear() - id);
+    Vehiculo.findAll({
+        include: [{
+                model: Propietario,
+                where: { FECHANACIMIENTO: {
+                        [Op.lt]: date.toISOString() } }
+            },
+            {
+                model: Modelo
             }
-            for (var i = 0; i < respuesta.length; i++) {
-
-                var hoy = new Date();
-                var cumpleanos = new Date(respuesta[i].FECHANACIMIENTO);
-                var edad = hoy.getFullYear() - cumpleanos.getFullYear();
-                var m = hoy.getMonth() - cumpleanos.getMonth();
-
-                if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
-                    edad--;
-                }
-                console.log(edad)
-                console.log(id)
-
-
-
-                if (id < edad) {
-
-                    Vehiculo.findAll({
-                        where: {
-                            PROPIETARIO: respuesta[i].CEDULA
-
-                        }
-                    }).then(buscaux => {
-                        //
-                        for (var j = 0; j < buscaux.length; j++) {
-                        resultado.push(buscaux[j]);
-                        console.log(buscaux[j])
-                        }
-                        
-                        
-                    })
-                }
-              
-            }
-            
-            if (resultado.length != 0) {
-
-                res.json(resultado);
-            } else {
-                res.json(resultado);
-                // return res.status().json();
-
-            }
-           
-        })
+        ]
+    }).then(vehiculo => {
+        if (vehiculo[0] == undefined) {
+            res.status(404).json({
+                message: "vehículo no encontrado"
+            });
+        } else {
+            res.json(vehiculo);
+        }
+    }).catch(err => {
+        console.log(err);
+        res.status(500).send("Error en la operación");
+    })
 
 })
 
-app.post('/app/vehiculo', function (req, res) {
+app.post('/app/vehiculo', function(req, res) {
     let body = req.body;
     Propietario.findAll({
-        where: {
-            CEDULA: body.PROPIETARIO.CEDULA
+            where: {
+                CEDULA: body.PROPIETARIO.CEDULA
 
-        }
-    })
+            }
+        })
         .then(busca => {
             if (busca.length === 0) {
                 Propietario.create({
-                    CEDULA: body.PROPIETARIO.CEDULA,
-                    NOMBRE: body.PROPIETARIO.NOMBRE,
-                    FECHANACIMIENTO: body.PROPIETARIO.FECHANACIMIENTO
+                        CEDULA: body.PROPIETARIO.CEDULA,
+                        NOMBRE: body.PROPIETARIO.NOMBRE,
+                        FECHANACIMIENTO: body.PROPIETARIO.FECHANACIMIENTO
 
-                })
+                    })
                     .then((created) => {
 
                         return res.json(created);
@@ -170,14 +145,14 @@ app.post('/app/vehiculo', function (req, res) {
                     if ((body.TRANSMISION == "MAN") || (body.TRANSMISION == "AUT")) {
                         if (body.PLACA.length <= 7) {
                             Vehiculo.create({
-                                PLACA: body.PLACA,
-                                CODIGO: body.CODIGO,
-                                PROPIETARIO: body.PROPIETARIO.CEDULA,
-                                CODIGOMODELO: body.CODIGOMODELO,
-                                ANIO: body.ANIO,
-                                MOTOR: body.MOTOR,
-                                TRANSMISION: body.TRANSMISION
-                            })
+                                    PLACA: body.PLACA,
+                                    CODIGO: body.CODIGO,
+                                    PROPIETARIO: body.PROPIETARIO.CEDULA,
+                                    CODIGOMODELO: body.CODIGOMODELO,
+                                    ANIO: body.ANIO,
+                                    MOTOR: body.MOTOR,
+                                    TRANSMISION: body.TRANSMISION
+                                })
                                 .then((created) => {
                                     return res.json(created);
 
